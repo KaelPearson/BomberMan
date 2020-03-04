7ae4e954d242818210158869cc097485d3068ee7
KaelPearson <kaelpearson1@gmail.com>
2020-03-04 00:02:52 -0800

    Added hud for playerOne

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -9,32 +9,32 @@ public class PowerUp : MonoBehaviour
         
         if(collider.gameObject.tag == "Player1") {
             if(obj.tag == "shoe") {
-                if (Player1Stats.MovementSpeed <= 6) {
+                if (Player1Stats.MovementSpeed < 7) {
                     Player1Stats.MovementSpeed += 1f;
-                } else {
-                    Debug.Log("Max");
-                }
+                } 
             } else if (obj.tag == "bombSize") {
-                if (Player1Stats.BombSize <= 5) {
+                if (Player1Stats.BombSize < 6) {
                     Player1Stats.BombSize += 1;
-                } else {
-                    Debug.Log("Max");
-                }
+                } 
             } else {
-                if (Player1Stats.BombAvaiable <= 5) {
+                if (Player1Stats.BombAvaiable < 6) {
                     Player1Stats.BombAvaiable += 1;
-                } else {
-                    Debug.Log("Max");
                 }
             }
             Destroy(gameObject);
         } else if(collider.gameObject.tag == "Player2") {
             if (obj.tag == "shoe") {
-                Player2Stats.MovementSpeed += 1;
+                if (Player2Stats.MovementSpeed < 7) {
+                    Player2Stats.MovementSpeed += 1;
+                }
             } else if (obj.tag == "bombSize") {
-                Player2Stats.BombSize += 1;
+                if (Player2Stats.BombSize < 6) {
+                    Player2Stats.BombSize += 1;
+                }
             } else {
-                Player2Stats.BombAvaiable += 1;
+                if (Player2Stats.BombAvaiable < 6) {
+                    Player2Stats.BombAvaiable += 1;
+                }
             }
             Destroy(gameObject);
         }
---
+++ Assets/Scripts/UpdateText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UpdateText : MonoBehaviour
+{
+    public Text text;
+    float p1MS = 3;
+    int p1BS = 1;
+    int p1BA = 1;
+    void Update()
+    {
+        p1MS = Player1Stats.MovementSpeed;
+        p1BS = Player1Stats.BombSize;
+        p1BA = Player1Stats.BombAvaiable;
+        text.text = "PlayerOne Movespeed : " + p1MS + "/7 || Bomb Size : " + p1BS + "/6 || Bomb Available : " + p1BA + "/6";
+    }
+}
